5d20e065e4902d75f1715dc5cac7f857773ee480
Ngansan <21521174@gm.uit.edu.vn>
2024-02-04 09:05:35 +0700

    changed

--- js/quizzes.js
+++ js/quizzes.js
@@ -1,42 +1,42 @@
 const quizData = [
     {
-        question: "このゲームをしたいけれど、____が分からない。",
-        a: "やりこと",
-        b: "やりかた",
-        c: "遊ぶこと",
-        d: "遊ぶ方",
-        correct: "b",
-    },
-    {
-        question: "_____が悪いんです。早く帰ってもいいですか。",
-        a: "体",
-        b: "げんき",
-        c: "くうき",
-        d: "気分",
-        correct: "d",
-    },
-    {
-        question: "あのう、ここに ____　いいですか。",
-        a: "座っても",
-        b: "触っても",
-        c: "回っても",
-        d: "割って",
+        question: "問題 1: ケネディ殺害の容疑者は（　　　）に謎を残したままマフィアに撃たれて死亡した。",
+        a: "動機",
+        b: "本音",
+        c: "動力",
+        d: "下心",
         correct: "a",
     },
     {
-        question: "くつをぬいで、スリッパに ____ください",
-        a: "ぬぎかえて",
-        b: "いれあけて",
-        c: "きがえて",
-        d: "はきかえて",
+        question: " 問題 2:いつ見つけても（　　　）の早いがんでは予後が悪く、遅いがんは予後がいい。早くても遅くても意味はないのです。",
+        a: "先進",
+        b: "増進",
+        c: "進出",
+        d: "進行",
         correct: "d",
     },
     {
-        question: "田中さん、このごろとても ____ね。",
-        a: "きれいになりました",
-        b: "きれいにしました",
-        c: "きれいな人です",
-        d: "きれいだったです",
+        question: " 問題 3: インターネット広告（　　　）と広告の効果の関係について考えてみよう。",
+        a: "値",
+        b: "費",
+        c: "料",
+        d: "額",
+        correct: "b",
+    },
+    {
+        question: " 問題 4:この数年間で千葉や隣接県では女児連れ去り事件が多発していた。まだ（　　　）解決の案件もあるが、未遂に終わり、容疑者が逮捕されたケースもある。",
+        a: "非",
+        b: "双",
+        c: "未",
+        d: "無",
+        correct: "c",
+    },
+    {
+        question: "問題 5: 辞書を引いたら、最初に（　　　）されている語義だけでなく、すべての語義をざっと確認する習慣を付けましょう。",
+        a: "記載",
+        b: "援用",
+        c: "参照",
+        d: "出典",
         correct: "a",
     },
 ];
@@ -91,7 +91,17 @@ submitBtn.addEventListener('click', () =>{
         }else{
             quiz.innerHTML = `
             <h2> You answered ${score}/${quizData.length} questions correctly </h2>
-            <button onclick="location.reload()"> Reload</button>`
+            <button 
+            onclick="location.reload()"
+            style="color: white; 
+            width: 100%;
+            border: none;
+            display: block;
+            cursor: pointer;
+            font-size: 1.1rem;
+            font-family: inherit;
+            padding: 1.3rem;
+            background-color: rgb(38, 92, 240);"> Reload</button>`
         }
     }
 })
